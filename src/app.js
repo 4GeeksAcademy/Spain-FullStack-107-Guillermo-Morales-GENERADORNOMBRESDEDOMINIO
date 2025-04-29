064cb485973109ee3bf336db24936222c01b53e6
GuillermoMoralesS <guillermorales2000@gmail.com>
2025-04-29 11:26:14 +0000

    GENERADORDOMINIOS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ 
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon',"puedes"];
+  let extens=[".com", ".net", ".us",".es"];
+
+  for (let p=0;p<pronoun.length;p++){
+    for(let a=0;a<adj.length;a++){
+      for(let n=0;n<noun.length;n++){
+        for(let e=0;e<extens.length;e++){
+
+          let dominio=pronoun[p]+adj[a]+noun[n]+extens[e];
+          console.log(dominio);
+        }
+      }
+    }
+  }
 };
+
